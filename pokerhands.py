9020ea02884ae0473ab15e38b9629501f03387d0
Deni Persson <deni.persson@gmail.com>
2022-09-15 16:23:51 +0200

    Update pokerhands.py

--- pokerhands.py
+++ pokerhands.py
@@ -1,7 +1,7 @@
 import math
 import random
 from itertools import combinations
-
+from operator import attrgetter
 
 """ 
 
@@ -27,7 +27,7 @@ class Card:
         self.number = n
     
     def __repr__(self):
-        return f"{self.color} + " ", {self.number}"
+        return f"{self.color},  {self.number}"
 
 
 def main():
@@ -36,12 +36,12 @@ def main():
 
     hand = pull_cards_from_deck(deck, 2)
 
-    print_cards(hand, "Hand: ")
+    #print_cards(hand, "Hand: ")
     community_cards = pull_cards_from_deck(deck, 3)
 
-    print_cards(community_cards, "Community cards: ")
+    #print_cards(community_cards, "Community cards: ")
 
-    print("Pair:", check_same_number(community_cards+hand, 2))
+    #print("Pair:", check_same_number(community_cards+hand, 2))
 
 
     comb = combinations(deck, 2)
@@ -49,6 +49,10 @@ def main():
     # Print the obtained combinations
     for i in list(comb):
         print (i)
+
+    test_flush_deck = [Card("hjärter", 1),Card("poop", 10),Card("hjärter", 11),Card("poop", 12),Card("hjärter", 13),Card("hjärter", 5), Card("hjärter", 3)]
+
+    print(check_straight(test_flush_deck))
 
 def print_cards(cards, description):
 
@@ -59,9 +63,9 @@ def print_cards(cards, description):
 
     print("\n")
 
+
 def check_same_number(cards, amount):
     pulled_cards = []
-    found = 1
     length = len(cards)
 
     for i in range(0, length-1):
@@ -70,14 +74,73 @@ def check_same_number(cards, amount):
 
         for j in range(0, len(pulled_cards)-1):
             if pulled_cards[j].number == cards[0].number:
-                found+= 1
+                return 1
     
-    print("Found:",found)
+    return 0
 
-    if found >= amount:
-        return True
+def check_flush(combinations):
+
+    flush = 0
+
+    for i in range(len(combinations)):
+        same_suit = 1
+        for j in range(i+1, len(combinations)):
+            if(combinations[i].color == combinations[j].color):
+                same_suit += 1
+            
+            if(same_suit >= 5):
+                flush = 1
+                break   
+
+    return flush
+
+def check_straight(combinations):
+    combinations.sort(key= attrgetter('number'))
+
+    found = 0
+
+    for i in range(len(combinations)):
+        straight = 1
+        for j in range(i+1, len(combinations)):
+
+            if combinations[j].number != combinations[i].number + j - i:
+                break
+            else:
+                straight += 1
+                if(combinations[j].number == 13 and straight == 4):
+                    if(combinations[0].number == 1):
+                        found = 1
+                        break
+
+            if(straight >= 5):
+                found = 1
+                break
+
+    return found
+
+def check_straight_flush(combinations):
+    if(check_straight(combinations) == 1 and check_flush(combinations)== 1):
+        return 1
     else:
-        return False
+        return 0
+
+def royal_straight_flush(combinations):
+    king_exists = False
+    s_exists = False
+
+    for i in range(len(combinations)):
+        if(combinations[i].number == 1):
+            s_exists = True
+        if(combinations[i].number == 13):
+            king_exists = True
+    
+    if(king_exists and s_exists and check_straight_flush(combinations) == 1):
+        return 1
+    
+    return 0
+
+
+
 
 def generate_deck():
     colors = ["hjärter", "spader", "ruter", "klöver"]
